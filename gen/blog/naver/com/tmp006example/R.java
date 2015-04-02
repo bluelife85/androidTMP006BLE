1f88ff7c1f3938b570449d2de8cf54a6174cdd58
bluelife85 <bluelife85@naver.com>
2015-04-02 15:02:46 +0900

    TMP006

--- gen/blog/naver/com/tmp006example/R.java
+++ gen/blog/naver/com/tmp006example/R.java
@@ -25,17 +25,23 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
-        public static final int scan_button=0x7f080000;
-        public static final int value_measurement_temperature=0x7f080006;
-        public static final int value_tamb=0x7f080004;
+        public static final int action_settings=0x7f08000b;
+        public static final int btn_scan_cancel=0x7f080009;
+        public static final int deviceList=0x7f08000a;
+        public static final int device_address=0x7f080007;
+        public static final int device_name=0x7f080006;
+        public static final int progress_spinner=0x7f080008;
+        public static final int txt_tamb=0x7f080001;
+        public static final int txt_temperature=0x7f080004;
+        public static final int txt_vobj=0x7f080000;
+        public static final int value_tamb=0x7f080003;
+        public static final int value_temperature=0x7f080005;
         public static final int value_vobj=0x7f080002;
-        public static final int view_measure_text=0x7f080005;
-        public static final int view_tamb_text=0x7f080003;
-        public static final int view_vobj_text=0x7f080001;
     }
     public static final class layout {
         public static final int activity_tmp006=0x7f030000;
+        public static final int dev_format=0x7f030001;
+        public static final int dev_list=0x7f030002;
     }
     public static final class menu {
         public static final int tmp006=0x7f070000;
@@ -45,9 +51,11 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int default_measured_temperature=0x7f050008;
         public static final int defualt_value=0x7f050005;
+        public static final int get_measure_value=0x7f050009;
         public static final int hello_world=0x7f050001;
         public static final int measure_text_view=0x7f050007;
         public static final int scan_button=0x7f050003;
+        public static final int scan_cancel=0x7f05000a;
         public static final int tamb_text_view=0x7f050006;
         public static final int vobj_text_view=0x7f050004;
     }
