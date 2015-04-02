1bc84700d209822cba79890449d7cfae1201397a
bluelife85 <bluelife85@naver.com>
2015-04-02 12:25:23 +0900

    activity layout created

--- gen/blog/naver/com/tmp006example/R.java
+++ gen/blog/naver/com/tmp006example/R.java
@@ -25,7 +25,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080007;
+        public static final int scan_button=0x7f080000;
+        public static final int value_measurement_temperature=0x7f080006;
+        public static final int value_tamb=0x7f080004;
+        public static final int value_vobj=0x7f080002;
+        public static final int view_measure_text=0x7f080005;
+        public static final int view_tamb_text=0x7f080003;
+        public static final int view_vobj_text=0x7f080001;
     }
     public static final class layout {
         public static final int activity_tmp006=0x7f030000;
@@ -36,7 +43,13 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
+        public static final int default_measured_temperature=0x7f050008;
+        public static final int defualt_value=0x7f050005;
         public static final int hello_world=0x7f050001;
+        public static final int measure_text_view=0x7f050007;
+        public static final int scan_button=0x7f050003;
+        public static final int tamb_text_view=0x7f050006;
+        public static final int vobj_text_view=0x7f050004;
     }
     public static final class style {
         /** 
